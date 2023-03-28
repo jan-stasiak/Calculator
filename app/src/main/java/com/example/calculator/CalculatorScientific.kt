57a37addc99cb9fe8a47ff1da00f5ed712532df3
jan-stasiak <jan_stasiak@outlook.com>
2023-03-28 18:18:25 +0200

    feat: add trigonometric functions

--- app/src/main/java/com/example/calculator/CalculatorScientific.kt
+++ app/src/main/java/com/example/calculator/CalculatorScientific.kt
@@ -1,13 +1,16 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.tan
 
 class CalculatorScientific : AppCompatActivity() {
     enum class MathOperation {
@@ -16,6 +19,12 @@ class CalculatorScientific : AppCompatActivity() {
         MULTIPLICATION,
         DIVISION,
         NONE
+    }
+
+    enum class MathTrigonometric {
+        SIN,
+        COS,
+        TAN
     }
 
     fun makeToast(vararg texts: String) {
@@ -49,25 +58,45 @@ class CalculatorScientific : AppCompatActivity() {
         val firstNum = previewString.replace(',', '.').toBigDecimal()
         val secondNum = firstString.replace(',', '.').toBigDecimal()
 
-        val s = if (operation == CalculatorSimple.MathOperation.DIVISION && secondNum == BigDecimal.ZERO) {
-            makeToast("Nie dziel przez 0!")
-            return "err"
-        } else {
-            val sum = when (operation) {
-                CalculatorSimple.MathOperation.MULTIPLICATION -> firstNum * secondNum
-                CalculatorSimple.MathOperation.ADDITION -> firstNum + secondNum
-                CalculatorSimple.MathOperation.SUBTRACTION -> firstNum - secondNum
-                CalculatorSimple.MathOperation.DIVISION -> firstNum.divide(secondNum, 8, RoundingMode.HALF_UP)
-                    .stripTrailingZeros()
-                CalculatorSimple.MathOperation.NONE -> return ""
+        val s =
+            if (operation == CalculatorSimple.MathOperation.DIVISION && secondNum == BigDecimal.ZERO) {
+                makeToast("Nie dziel przez 0!")
+                return "err"
+            } else {
+                val sum = when (operation) {
+                    CalculatorSimple.MathOperation.MULTIPLICATION -> firstNum * secondNum
+                    CalculatorSimple.MathOperation.ADDITION -> firstNum + secondNum
+                    CalculatorSimple.MathOperation.SUBTRACTION -> firstNum - secondNum
+                    CalculatorSimple.MathOperation.DIVISION -> firstNum.divide(
+                        secondNum,
+                        8,
+                        RoundingMode.HALF_UP
+                    )
+                        .stripTrailingZeros()
+                    CalculatorSimple.MathOperation.NONE -> return ""
+                }
+                val df = DecimalFormat("#.####################")
+                df.format(sum.stripTrailingZeros()).toString().replace('.', ',')
             }
-            val df = DecimalFormat("#.####################")
-            df.format(sum.stripTrailingZeros()).toString().replace('.', ',')
-        }
 
         mFirstNumber.text = s
         mPreview.text = ""
         return s
+    }
+
+    fun trigonometric_calc(function: MathTrigonometric, firstString: String): String {
+        this.operation = CalculatorSimple.MathOperation.NONE
+        if (firstString.isNotEmpty()) {
+            var value = firstString.replace(',', '.').toBigDecimal()
+            when(function) {
+                MathTrigonometric.SIN->value = sin(value.toDouble()).toBigDecimal()
+                MathTrigonometric.COS->value = cos(value.toDouble()).toBigDecimal()
+                MathTrigonometric.TAN->value = tan(value.toDouble()).toBigDecimal()
+            }
+            val df = DecimalFormat("#.########")
+            return df.format(value.stripTrailingZeros()).toString().replace('.', ',')
+        }
+        return "0"
     }
 
     lateinit var mFirstNumber: TextView
@@ -127,63 +156,66 @@ class CalculatorScientific : AppCompatActivity() {
         val mButtonSub = findViewById<Button>(R.id.buttonSub)
         val mButtonMul = findViewById<Button>(R.id.buttonMul)
         val mButtonDiv = findViewById<Button>(R.id.buttonDiv)
+        val mButtonSin = findViewById<Button>(R.id.buttonSin)
+        val mButtonCos = findViewById<Button>(R.id.buttonCos)
+        val mButtonTan = findViewById<Button>(R.id.buttonTan)
 
         mButton0.setOnClickListener {
             if (firstString != "0") firstString += "0"
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton1.setOnClickListener {
             firstString = addNumberToString(firstString, 1)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton2.setOnClickListener {
             firstString = addNumberToString(firstString, 2)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton3.setOnClickListener {
             firstString = addNumberToString(firstString, 3)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton4.setOnClickListener {
             firstString = addNumberToString(firstString, 4)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton5.setOnClickListener {
             firstString = addNumberToString(firstString, 5)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton6.setOnClickListener {
             firstString = addNumberToString(firstString, 6)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton7.setOnClickListener {
             firstString = addNumberToString(firstString, 7)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton8.setOnClickListener {
             firstString = addNumberToString(firstString, 8)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButton9.setOnClickListener {
             firstString = addNumberToString(firstString, 9)
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButtonAC.setOnClickListener {
             firstString = "0"
             previewString = ""
             isComma = false
-            mFirstNumber.setText(firstString)
-            mPreview.setText(previewString)
+            mFirstNumber.text = firstString
+            mPreview.text = previewString
         }
 
         mButtonBksp.setOnClickListener {
@@ -195,7 +227,7 @@ class CalculatorScientific : AppCompatActivity() {
             if (firstString.length == 1 && firstString.get(0) == '-') {
                 firstString = "0"
             }
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButtonComma.setOnClickListener {
@@ -204,7 +236,7 @@ class CalculatorScientific : AppCompatActivity() {
                 isComma = true
                 ","
             }
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButtonSign.setOnClickListener {
@@ -215,7 +247,7 @@ class CalculatorScientific : AppCompatActivity() {
                     "-$firstString"
                 }
             }
-            mFirstNumber.setText(firstString)
+            mFirstNumber.text = firstString
         }
 
         mButtonPlus.setOnClickListener {
@@ -227,8 +259,8 @@ class CalculatorScientific : AppCompatActivity() {
                 previewString = firstString
                 firstString = "0"
                 operation = CalculatorSimple.MathOperation.ADDITION
-                mFirstNumber.setText("0")
-                mPreview.setText(previewString)
+                mFirstNumber.text = "0"
+                mPreview.text = previewString
             }
         }
 
@@ -241,8 +273,8 @@ class CalculatorScientific : AppCompatActivity() {
                 previewString = firstString
                 firstString = "0"
                 operation = CalculatorSimple.MathOperation.SUBTRACTION
-                mFirstNumber.setText("0")
-                mPreview.setText(previewString)
+                mFirstNumber.text = "0"
+                mPreview.text = previewString
             }
         }
 
@@ -255,8 +287,8 @@ class CalculatorScientific : AppCompatActivity() {
                 previewString = firstString
                 firstString = "0"
                 operation = CalculatorSimple.MathOperation.MULTIPLICATION
-                mFirstNumber.setText("0")
-                mPreview.setText(previewString)
+                mFirstNumber.text = "0"
+                mPreview.text = previewString
             }
 
         }
@@ -274,8 +306,8 @@ class CalculatorScientific : AppCompatActivity() {
                     previewString = firstString
                     firstString = "0"
                     operation = CalculatorSimple.MathOperation.DIVISION
-                    mFirstNumber.setText("0")
-                    mPreview.setText(previewString)
+                    mFirstNumber.text = "0"
+                    mPreview.text = previewString
                 }
             }
         }
@@ -294,6 +326,27 @@ class CalculatorScientific : AppCompatActivity() {
                 }
                 makeToast("Equal", firstString, previewString)
             }
+        }
+
+        mButtonSin.setOnClickListener {
+            firstString = trigonometric_calc(MathTrigonometric.SIN, firstString)
+            mFirstNumber.text = firstString
+            previewString = ""
+            mPreview.text = previewString
+        }
+
+        mButtonCos.setOnClickListener {
+            firstString = trigonometric_calc(MathTrigonometric.COS, firstString)
+            mFirstNumber.text = firstString
+            previewString = ""
+            mPreview.text = previewString
+        }
+
+        mButtonTan.setOnClickListener {
+            firstString = trigonometric_calc(MathTrigonometric.TAN, firstString)
+            mFirstNumber.text = firstString
+            previewString = ""
+            mPreview.text = previewString
         }
     }
 }
